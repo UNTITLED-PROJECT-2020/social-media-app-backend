23cc70de1ca8dd103c5b950a7e7907ac9e1a1ecf
angadsinghsandhu <angadsandhuworkmail@gmail.com>
2020-09-18 08:20:30 +0530

    base dir changed

---
+++ profileDetails/urls.py
@@ -0,0 +1,13 @@
+# imports
+from . import views
+from django.urls import include, path, re_path
+from django.shortcuts import render
+
+
+# connected to '/'
+app_name = 'profileDetails'
+
+
+urlpatterns = [
+    # path('', views.home, name="home"),
+]
