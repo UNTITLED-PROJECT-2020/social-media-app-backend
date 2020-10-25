4a6aa47be47385e1e1ff45f3204c4048a230aecf
Angad Sandhu <55819847+angadsinghsandhu@users.noreply.github.com>
2020-10-25 11:34:04 +0530

    heroku updates

--- AppBackend/routing.py
+++ AppBackend/routing.py
@@ -3,6 +3,8 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
+print("hellooooo")
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
