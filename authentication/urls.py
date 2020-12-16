5acaad67457983019db42ce8a3f83250c567a853
Angad Sandhu <55819847+angadsinghsandhu@users.noreply.github.com>
2020-12-16 17:57:34 +0530

    updated urls.py of auth app

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,14 +8,23 @@ from rest_framework.authtoken.views import obtain_auth_token
 # connected to 'auth/'
 app_name = 'authentication'
 
+# dictionary of all the generic methods
+method_dict = {
+    #     'get': 'list',
+    'post': 'create',
+    # 'put': 'update',
+    # 'patch': 'retrieve',
+    # 'delete': 'delete',
+}
+
 # urls
 urlpatterns = [
     # returning token using only (username, password)
     path('login/', obtain_auth_token),
 
     # signing up
-    path('signup/', views.GenericSignupViewSet, name="signup"),
+    path('signup/', views.GenericSignupViewSet.as_view(method_dict), name="signup"),
 
     # returning data
-    path('loginData/', views.GenericLoginViewSet, name="login"),
+    path('loginData/', views.GenericLoginViewSet.as_view(method_dict), name="login"),
 ]
